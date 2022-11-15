dfa25f99dd62ecd0887fffa863503e9802c85e8d
Uladzislau Hubar <hubar.uladzislau@gmail.com>
2022-11-15 11:26:44 +0100

    Replaced wrong setter

--- migrations/2_total_migration.js
+++ migrations/2_total_migration.js
@@ -131,7 +131,7 @@ module.exports = async (deployer, network, accounts) => {
                 .then((result) => {
                     contentAsset = result;
                 });
-            await hub.setContractAddress('ContentAsset', contentAsset.address);
+            await hub.setAssetContractAddress('ContentAsset', contentAsset.address);
             /* ---------------------------------------------------------------------------------------- */
 
             /* ---------------------------------------ERC20-------------------------------------------- */
