b5e7132a5a86d2240b687b86badc56ab954b730b
Uladzislau Hubar <hubar.uladzislau@gmail.com>
2022-12-07 14:41:07 +0100

    Removed junk, added ignores, updated dependencies and modules exports

--- index.js
+++ index.js
@@ -1,19 +1,45 @@
 const hub = require("./build/contracts/Hub.json");
-const uaiRegistry = require("./build/contracts/UAIRegistry.json");
-const assertionRegistry = require("./build/contracts/AssertionRegistry.json");
-const assetRegistry = require("./build/contracts/AssetRegistry.json");
+const parametersStorage = require("./build/contracts/ParametersStorage.json");
+const hashingProxy = require("./build/contracts/HashingProxy.json");
+const sha256Contract = require("./build/contracts/SHA256.json");
+const scoringProxy = require("./build/contracts/ScoringProxy.json");
+const log2pldsfContract = require("./build/contracts/Log2PLDSF.json");
+const shardingTableStorage = require("./build/contracts/ShardingTableStorage.json");
+const shardingTableContract = require("./build/contracts/ShardingTable.json");
+const assertionStorage = require("./build/contracts/AssertionStorage.json");
+const assertionContract = require("./build/contracts/Assertion.json");
+const serviceAgreementStorageV1 = require("./build/contracts/ServiceAgreementStorageV1.json");
+const serviceAgreementContractV1 = require("./build/contracts/ServiceAgreementV1.json");
+const contentAsset = require("./build/contracts/ContentAsset.json");
 const erc20Token = require("./build/contracts/ERC20Token.json");
-const identity = require("./build/contracts/Identity.json");
-const profile = require("./build/contracts/Profile.json");
+const identityStorage = require("./build/contracts/IdentityStorage.json");
+const identityContract = require("./build/contracts/Identity.json");
 const profileStorage = require("./build/contracts/ProfileStorage.json");
+const profileContract = require("./build/contracts/Profile.json");
+const stakingStorage = require("./build/contracts/StakingStorage.json");
+const stakingContract = require("./build/contracts/Staking.json");
+const whitelistStorage = require("./build/contracts/WhitelistStorage.json");
 
 module.exports = {
   hub,
-  uaiRegistry,
-  assertionRegistry,
-  assetRegistry,
+  parametersStorage,
+  hashingProxy,
+  sha256Contract,
+  scoringProxy,
+  log2pldsfContract,
+  shardingTableStorage,
+  shardingTableContract,
+  assertionStorage,
+  assertionContract,
+  serviceAgreementStorageV1,
+  serviceAgreementContractV1,
+  contentAsset,
   erc20Token,
-  identity,
-  profile,
+  identityStorage,
+  identityContract,
   profileStorage,
+  profileContract,
+  stakingStorage,
+  stakingContract,
+  whitelistStorage
 };
