bbf78d963b46a63c4c55fd6a82fc0bbdb59c40bc
Uladzislau Hubar <hubar.uladzislau@gmail.com>
2024-06-17 12:22:43 +0200

    Deployed Paranet contracts on Chiado Devnet, redeployed Paranet logic on Neuroweb Devnet

--- deploy/024_deploy_paranets_registry.ts
+++ deploy/024_deploy_paranets_registry.ts
@@ -2,10 +2,6 @@ import { DeployFunction } from 'hardhat-deploy/types';
 import { HardhatRuntimeEnvironment } from 'hardhat/types';
 
 const func: DeployFunction = async function (hre: HardhatRuntimeEnvironment) {
-  if (!hre.network.name.startsWith('otp') && !hre.network.name.startsWith('hardhat')) {
-    return;
-  }
-
   await hre.helpers.deploy({
     newContractName: 'ParanetsRegistry',
   });
