49c38c6b366e772d5c555f47eaf03368c49a07fe
Uladzislau Hubar <hubar.uladzislau@gmail.com>
2024-12-12 16:12:42 +0100

    Added onlyHubOwner modifier

--- contracts/abstract/HubDependent.sol
+++ contracts/abstract/HubDependent.sol
@@ -18,16 +18,25 @@ abstract contract HubDependent {
         _;
     }
 
+    modifier onlyHubOwner() {
+        _checkHubOwner();
+        _;
+    }
+
     modifier onlyContracts() {
         _checkHubContract();
         _;
     }
 
     function _checkHub() internal view virtual {
-        require(msg.sender == address(hub), "Fn can only be used by hub owner");
+        require(msg.sender == address(hub), "Fn can only be used by hub");
+    }
+
+    function _checkHubOwner() internal view virtual {
+        require(msg.sender == hub.owner(), "Fn can only be used by hub owner");
     }
 
     function _checkHubContract() internal view virtual {
-        require(hub.isContract(msg.sender), "Fn can only be called by the hub");
+        require(hub.isContract(msg.sender), "Fn can only be called by the hub contracts");
     }
 }
