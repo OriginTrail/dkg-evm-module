7d347309c918ee77b21f29f7e47731027cc6ab72
Uladzislau Hubar <hubar.uladzislau@gmail.com>
2024-12-13 02:22:17 +0100

    Changed signatures verification method, removed unnecessary arguments

--- contracts/libraries/KnowledgeCollectionLib.sol
+++ contracts/libraries/KnowledgeCollectionLib.sol
@@ -58,21 +58,10 @@ library KnowledgeCollectionLib {
     event URIUpdate(string newURI);
 
     error InvalidTokenAmount(uint96 expectedTokenAMount, uint96 tokenAmount);
-    error InvalidPublisherNodeSignature(uint72 identityId, address signer, bytes32 messageHash, bytes32 r, bytes32 vs);
-    error InvalidReplicationSignatures(
-        uint72[] identityIds,
-        address[] signers,
-        bytes32 messageHash,
-        bytes32[] r,
-        bytes32[] vs
-    );
+    error InvalidSignature(uint72 identityId, bytes32 messageHash, bytes32 r, bytes32 vs);
+    error SignerIsNotNodeOperator(uint72 identityId, address signer);
     error KnowledgeCollectionExpired(uint256 id, uint256 currentEpoch, uint256 endEpoch);
     error NotPartOfKnowledgeCollection(uint256 id, uint256 tokenId);
-    error SignaturesSignersMismatch(
-        uint256 rAmount,
-        uint256 vsAmount,
-        uint256 identityIdsAmount,
-        uint256 signersAmount
-    );
+    error SignaturesSignersMismatch(uint256 rAmount, uint256 vsAmount, uint256 identityIdsAmount);
     error MinSignaturesRequirementNotMet(uint256 requiredSignatures, uint256 receivedSignatures);
 }
