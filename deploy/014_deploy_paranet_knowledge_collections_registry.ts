f97063a95c94d7443aa324101037baf14fe43ed7
Mihajlo Pavlovic <mihajlo.pavlovic@origin-trail.com>
2025-01-20 15:27:59 +0100

    Update deployment script & ABIs exports

--- deploy/014_deploy_paranet_knowledge_collections_registry.ts
+++ deploy/014_deploy_paranet_knowledge_collections_registry.ts
@@ -2,14 +2,11 @@ import { HardhatRuntimeEnvironment } from 'hardhat/types';
 import { DeployFunction } from 'hardhat-deploy/types';
 
 const func: DeployFunction = async function (hre: HardhatRuntimeEnvironment) {
-  // TODO: Remove when paranets V2 are finished
-  return;
-
   await hre.helpers.deploy({
-    newContractName: 'ParanetKnowledgeAssetsRegistry',
+    newContractName: 'ParanetKnowledgeCollectionsRegistry',
   });
 };
 
 export default func;
-func.tags = ['ParanetKnowledgeAssetsRegistry'];
+func.tags = ['ParanetKnowledgeCollectionsRegistry'];
 func.dependencies = ['Hub'];
