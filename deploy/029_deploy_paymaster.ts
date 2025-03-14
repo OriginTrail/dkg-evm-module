dc7991240003222e36b1ce3904fa98bc2f23c9ca
Marko Kostic <marko03kostic@protonmail.com>
2025-03-14 11:30:27 +0100

    deployment script for Paymaster

--- deploy/029_deploy_paymaster.ts
+++ deploy/029_deploy_paymaster.ts
@@ -2,9 +2,30 @@ import { HardhatRuntimeEnvironment } from 'hardhat/types';
 import { DeployFunction } from 'hardhat-deploy/types';
 
 const func: DeployFunction = async function (hre: HardhatRuntimeEnvironment) {
-  await hre.helpers.deploy({
-    newContractName: 'Paymaster',
-  });
+  const { deployments, getNamedAccounts } = hre;
+  const { deployer } = await getNamedAccounts();
+
+  if (!hre.helpers.isDeployed('Paymaster')) {
+    if (hre.network.config.environment === 'development') {
+      hre.helpers.resetDeploymentsJson();
+      console.log('Hardhat deployments config reset.');
+    }
+
+    const hub = await deployments.get('Hub');
+
+    const paymasterDeploymentResult = await deployments.deploy('Paymaster', {
+      contract: 'Paymaster',
+      from: deployer,
+      args: [hub.address, deployer],
+      log: true,
+    });
+
+    await hre.helpers.updateDeploymentsJson(
+      'Paymaster',
+      paymasterDeploymentResult.address,
+      paymasterDeploymentResult.receipt!.blockNumber,
+    );
+  }
 };
 
 export default func;
