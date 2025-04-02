3e4361e4027622b2c303165d8d82318d1d0cde1d
Zvonimir <zvonimir@Zvonimirs-MacBook-Pro.local>
2025-04-02 15:28:20 +0200

    Add submitProof

--- contracts/RandomSampling.sol
+++ contracts/RandomSampling.sol
@@ -10,23 +10,46 @@ import {RandomSamplingLib} from "./libraries/RandomSamplingLib.sol";
 import {IdentityStorage} from "./storage/IdentityStorage.sol";
 import {RandomSamplingStorage} from "./storage/RandomSamplingStorage.sol";
 import {KnowledgeCollectionStorage} from "./storage/KnowledgeCollectionStorage.sol";
+import {StakingStorage} from "./storage/StakingStorage.sol";
+import {ProfileStorage} from "./storage/ProfileStorage.sol";
+import {EpochStorage} from "./storage/EpochStorage.sol";
+import {Chronos} from "./storage/Chronos.sol";
+import {AskStorage} from "./storage/AskStorage.sol";
 
-contract RandomSampling is INamed, IVersioned, ContractStatus, IInitializable {
+contract RandomSampling is INamed, IVersioned, ContractStatus {
     string private constant _NAME = "RandomSampling";
     string private constant _VERSION = "1.0.0";
+    uint8 public avgBlockTimeInSeconds;
 
     IdentityStorage public identityStorage;
     RandomSamplingStorage public rss;
     KnowledgeCollectionStorage public kcs;
+    StakingStorage public stakingStorage;
+    ProfileStorage public profileStorage;
+    EpochStorage public epochStorage;
+    Chronos public chronos;
+    AskStorage public askStorage;
 
     event ChallengeCreated(uint256 knowledgeCollectionId, uint256 chunkId, uint256 activeProofPeriodBlock);
+    event ValidProofSubmitted(uint72 indexed identityId, uint256 indexed epoch, uint256 score);
+    event AvgBlockTimeUpdated(uint256 indexed chainId, uint8 avgBlockTimeInSeconds);
+    event ProofingPeriodDurationInBlocksUpdated(uint8 durationInBlocks);
 
-    constructor(address hubAddress) ContractStatus(hubAddress) {}
+    constructor(address hubAddress, uint8 _avgBlockTimeInSeconds) ContractStatus(hubAddress) {
+        avgBlockTimeInSeconds = _avgBlockTimeInSeconds;
+    }
 
-    function initialize() external override {
+    function initialize(uint8 _proofingPeriodDurationInBlocks) external {
         identityStorage = IdentityStorage(hub.getContractAddress("IdentityStorage"));
         rss = RandomSamplingStorage(hub.getContractAddress("RandomSamplingStorage"));
         kcs = KnowledgeCollectionStorage(hub.getContractAddress("KnowledgeCollectionStorage"));
+        stakingStorage = StakingStorage(hub.getContractAddress("StakingStorage"));
+        profileStorage = ProfileStorage(hub.getContractAddress("ProfileStorage"));
+        epochStorage = EpochStorage(hub.getContractAddress("EpochStorage"));
+        chronos = Chronos(hub.getContractAddress("Chronos"));
+        askStorage = AskStorage(hub.getContractAddress("AskStorage"));
+
+        rss.setProofingPeriodDurationInBlocks(_proofingPeriodDurationInBlocks);
     }
 
     function name() external pure virtual override returns (string memory) {
@@ -41,25 +64,18 @@ contract RandomSampling is INamed, IVersioned, ContractStatus, IInitializable {
         // identityId
         uint72 identityId = identityStorage.getIdentityId(msg.sender);
 
-        // update the active proof period start block if necessary
-        uint8 chainProofingPeriodDurationInBlocks = rss.getChainProofingPeriodDurationInBlocks(block.chainid);
-        if (block.number > rss.getActiveProofPeriodStartBlock() + chainProofingPeriodDurationInBlocks) {
-            rss.setActiveProofPeriodStartBlock(block.number - (block.number % chainProofingPeriodDurationInBlocks));
-        }
-
         RandomSamplingLib.Challenge memory nodeChallenge = rss.getNodeChallenge(identityId);
 
-        // If node has already participated in the challenge, return an empty challenge
-        if (nodeChallenge.solved == true) {
-            return RandomSamplingLib.Challenge(0, 0, 0, false);
-        }
+        if (nodeChallenge.activeProofPeriodStartBlock == rss.getActiveProofPeriodStartBlock()) {
+            // If node has already solved the challenge for this period, return an empty challenge
+            if (nodeChallenge.solved == true) {
+                return RandomSamplingLib.Challenge(0, 0, 0, false);
+            }
 
-        // If the challenge for this node exists but has not been solved yet, return the existing challenge
-        if (
-            nodeChallenge.activeProofPeriodStartBlock == rss.getActiveProofPeriodStartBlock() &&
-            nodeChallenge.knowledgeCollectionId != 0
-        ) {
-            return nodeChallenge;
+            // If the challenge for this node exists but has not been solved yet, return the existing challenge
+            if (nodeChallenge.knowledgeCollectionId != 0) {
+                return nodeChallenge;
+            }
         }
 
         // Generate a new challenge
@@ -71,6 +87,82 @@ contract RandomSampling is INamed, IVersioned, ContractStatus, IInitializable {
         return challenge;
     }
 
+    function computeMerkleRoot(bytes32 chunk, bytes32[] memory merkleProof) public pure returns (bytes32) {
+        bytes32 computedHash = keccak256(abi.encodePacked(chunk));
+
+        for (uint256 i = 0; i < merkleProof.length; i++) {
+            if (computedHash < merkleProof[i]) {
+                computedHash = keccak256(abi.encodePacked(computedHash, merkleProof[i]));
+            } else {
+                computedHash = keccak256(abi.encodePacked(merkleProof[i], computedHash));
+            }
+        }
+
+        return computedHash;
+    }
+
+    function submitProof(bytes32 chunk, bytes32[] calldata merkleProof) public returns (bool) {
+        // Get node identityId
+        uint72 identityId = identityStorage.getIdentityId(msg.sender);
+
+        // update the active proof period start block if necessary
+        uint256 currentActiveBlock = rss.getActiveProofPeriodStartBlock();
+
+        // Get node challenge
+        RandomSamplingLib.Challenge memory challenge = rss.getNodeChallenge(identityId);
+
+        // verify that the challengeId matches the current challenge
+        if (challenge.activeProofPeriodStartBlock != currentActiveBlock) {
+            // This challenge is no longer active
+            return false;
+        }
+
+        // Construct the merkle root from chunk and merkleProof
+        bytes32 computedMerkleRoot = computeMerkleRoot(chunk, merkleProof);
+
+        // Get the expected merkle root for this challenge
+        bytes32 expectedMerkleRoot = kcs.getLatestMerkleRoot(challenge.knowledgeCollectionId);
+
+        // Verify the submitted root matches
+        if (computedMerkleRoot == expectedMerkleRoot) {
+            // Mark as correct submission and add points to the node
+            challenge.solved = true;
+            rss.setNodeChallenge(identityId, challenge);
+
+            uint256 epoch = chronos.getCurrentEpoch();
+            rss.incrementEpochNodeValidProofsCount(epoch, identityId);
+
+            uint256 SCALING_FACTOR = 1e18;
+
+            // Node stake factor
+            uint256 nodeStake = stakingStorage.getNodeStake(identityId);
+            uint256 nodeStakeFactor = (2 * ((nodeStake * SCALING_FACTOR) / 2000000) ** 2) / SCALING_FACTOR;
+
+            // Node ask factor
+            uint256 nodeAsk = profileStorage.getAsk(identityId);
+            (uint256 askLowerBound, uint256 askUpperBound) = askStorage.getAskBounds();
+            uint256 nodeAskFactor = (nodeStake *
+                (((askUpperBound - nodeAsk) * SCALING_FACTOR) / (askUpperBound - askLowerBound)) ** 2) /
+                2 /
+                SCALING_FACTOR;
+
+            // Node publishing factor
+            uint256 nodePubFactor = epochStorage.getNodeCurrentEpochProducedKnowledgeValue(identityId);
+            uint256 nodePublishingFactor = (nodeStakeFactor *
+                (nodePubFactor * epochStorage.getCurrentEpochNodeMaxProducedKnowledgeValue())) / SCALING_FACTOR;
+
+            // Node score
+            uint256 score = nodeStakeFactor + nodePublishingFactor - nodeAskFactor;
+            rss.addToEpochNodeTotalScore(epoch, identityId, score);
+
+            emit ValidProofSubmitted(identityId, epoch, score);
+
+            return true;
+        }
+
+        return false;
+    }
+
     function _generateChallenge(
         uint72 identityId,
         address originalSender
@@ -79,7 +171,7 @@ contract RandomSampling is INamed, IVersioned, ContractStatus, IInitializable {
 
         bytes32 pseudoRandomVariable = keccak256(
             abi.encodePacked(
-                block.prevrandao,
+                block.difficulty,
                 myBlockHash,
                 originalSender,
                 block.timestamp,
@@ -98,4 +190,21 @@ contract RandomSampling is INamed, IVersioned, ContractStatus, IInitializable {
 
         return RandomSamplingLib.Challenge(knowledgeCollectionId, chunkId, activeProofPeriodStartBlock, false);
     }
+
+    function getAllExpectedEpochProofsCount() internal view returns (uint256) {
+        uint256 allNodesCount = identityStorage.lastIdentityId();
+        uint256 epochLengthInSeconds = chronos.epochLength();
+        uint256 maxPossibleNodeProofsInEpoch = epochLengthInSeconds /
+            (rss.getProofingPeriodDurationInBlocks() * avgBlockTimeInSeconds);
+        return allNodesCount * maxPossibleNodeProofsInEpoch;
+    }
+
+    function setAvgBlockTimeInSeconds(uint8 blockTimeInSeconds) external onlyHubOwner {
+        avgBlockTimeInSeconds = blockTimeInSeconds;
+        emit AvgBlockTimeUpdated(block.chainid, blockTimeInSeconds);
+    }
+
+    // get rewards amount
+
+    // claim rewards
 }
--- contracts/storage/RandomSamplingStorage.sol
+++ contracts/storage/RandomSamplingStorage.sol
@@ -4,21 +4,27 @@ pragma solidity ^0.8.20;
 
 import {INamed} from "../interfaces/INamed.sol";
 import {IVersioned} from "../interfaces/IVersioned.sol";
-import {Guardian} from "../Guardian.sol";
 import {RandomSamplingLib} from "../libraries/RandomSamplingLib.sol";
+import {HubDependent} from "../abstract/HubDependent.sol";
 
-contract RandomSamplingStorage is INamed, IVersioned, Guardian {
+contract RandomSamplingStorage is INamed, IVersioned, HubDependent {
     string private constant _NAME = "RandomSamplingStorage";
     string private constant _VERSION = "1.0.0";
+    uint8 public constant CHUNK_BYTE_SIZE = 32;
 
-    uint8 public immutable CHUNK_BYTE_SIZE = 32;
+    uint8 public proofingPeriodDurationInBlocks;
+
     uint256 public activeProofPeriodStartBlock;
-    // Chain Id => Proofing period duration in blocks
-    mapping(uint256 => uint8) public chainProofingPeriodDurationInBlocks;
     // identityId => Challenge - used in proof to verify the challenge is within proofing period
     mapping(uint72 => RandomSamplingLib.Challenge) public nodesChallenges;
+    // epoch => identityId => successful proofs count
+    mapping(uint256 => mapping(uint72 => uint256)) public epochNodeValidProofsCount;
+    // epoch => identityId => score
+    mapping(uint256 => mapping(uint72 => uint256)) public epochNodeTotalScore;
+    // epoch => score
+    mapping(uint256 => uint256) public epochAllNodesTotalScore;
 
-    constructor(address hubAddress) Guardian(hubAddress) {}
+    constructor(address hubAddress) HubDependent(hubAddress) {}
 
     function name() external pure virtual override returns (string memory) {
         return _NAME;
@@ -28,16 +34,25 @@ contract RandomSamplingStorage is INamed, IVersioned, Guardian {
         return _VERSION;
     }
 
-    function getActiveProofPeriodStartBlock() external view returns (uint256) {
+    function getActiveProofPeriodStartBlock() external returns (uint256) {
+        if (block.number > activeProofPeriodStartBlock + proofingPeriodDurationInBlocks) {
+            uint256 newActiveBlock = block.number - (block.number % proofingPeriodDurationInBlocks);
+            activeProofPeriodStartBlock = newActiveBlock;
+        }
+
         return activeProofPeriodStartBlock;
     }
 
-    function getChainProofingPeriodDurationInBlocks(uint256 chainId) external view returns (uint8) {
-        return chainProofingPeriodDurationInBlocks[chainId];
+    function getProofingPeriodDurationInBlocks() external view returns (uint8) {
+        return proofingPeriodDurationInBlocks;
     }
 
-    function setChainProofingPeriodDurationInBlocks(uint8 chainId, uint8 durationInBlocks) external onlyHubOwner {
-        chainProofingPeriodDurationInBlocks[chainId] = durationInBlocks;
+    function setProofingPeriodDurationInBlocks(uint8 durationInBlocks) external {
+        require(
+            msg.sender == hub.owner() || msg.sender == hub.getContractAddress("RandomSampling"),
+            "Only hub owner or RandomSampling contract can call this function"
+        );
+        proofingPeriodDurationInBlocks = durationInBlocks;
     }
 
     function getNodeChallenge(uint72 identityId) external view returns (RandomSamplingLib.Challenge memory) {
@@ -51,8 +66,29 @@ contract RandomSamplingStorage is INamed, IVersioned, Guardian {
         nodesChallenges[identityId] = challenge;
     }
 
-    function setActiveProofPeriodStartBlock(uint256 blockNumber) external onlyRandomSamplingContract {
-        activeProofPeriodStartBlock = blockNumber;
+    function incrementEpochNodeValidProofsCount(uint256 epoch, uint72 identityId) external onlyRandomSamplingContract {
+        epochNodeValidProofsCount[epoch][identityId] += 1;
+    }
+
+    function getEpochNodeValidProofsCount(uint256 epoch, uint72 identityId) external view returns (uint256) {
+        return epochNodeValidProofsCount[epoch][identityId];
+    }
+
+    function getEpochNodeTotalScore(uint256 epoch, uint72 identityId) external view returns (uint256) {
+        return epochNodeTotalScore[epoch][identityId];
+    }
+
+    function addToEpochNodeTotalScore(
+        uint256 epoch,
+        uint72 identityId,
+        uint256 score
+    ) external onlyRandomSamplingContract {
+        epochNodeTotalScore[epoch][identityId] += score;
+        epochAllNodesTotalScore[epoch] += score;
+    }
+
+    function getEpochAllNodesTotalScore(uint256 epoch) external view returns (uint256) {
+        return epochAllNodesTotalScore[epoch];
     }
 
     modifier onlyRandomSamplingContract() {
