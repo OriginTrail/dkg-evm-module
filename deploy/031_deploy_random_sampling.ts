465f0adbada6d15e9d008bd4a1c750c1dd3ce913
Zvonimir <zvonimir@Zvonimirs-MacBook-Pro.local>
2025-04-04 12:20:41 +0200

    Add deployment scripts

---
+++ deploy/031_deploy_random_sampling.ts
@@ -0,0 +1,27 @@
+import { HardhatRuntimeEnvironment } from 'hardhat/types';
+import { DeployFunction } from 'hardhat-deploy/types';
+
+const func: DeployFunction = async function (hre: HardhatRuntimeEnvironment) {
+  const randomSamplingParametersConfig =
+    hre.helpers.parametersConfig[hre.network.config.environment].RandomSampling;
+
+  await hre.helpers.deploy({
+    newContractName: 'RandomSampling',
+    additionalArgs: [randomSamplingParametersConfig.avgBlockTimeInSeconds],
+  });
+};
+
+export default func;
+func.tags = ['RandomSampling'];
+func.dependencies = [
+  'Hub',
+  'Chronos',
+  'RandomSamplingStorage',
+  'StakingStorage',
+  'ProfileStorage',
+  'EpochStorageV8',
+  'AskStorage',
+  'DelegatorsInfo',
+  'KnowledgeCollectionStorage',
+  'IdentityStorage',
+];
