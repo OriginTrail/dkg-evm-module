defd9d36d460b306d75a9554704b623f477db454
Luka Golinar <luka.golinar@gmail.com>
2025-04-11 10:22:22 +0200

    feat: deployment version update

--- deploy/030_deploy_random_sampling_storage.ts
+++ deploy/030_deploy_random_sampling_storage.ts
@@ -16,4 +16,4 @@ const func: DeployFunction = async function (hre: HardhatRuntimeEnvironment) {
 
 export default func;
 func.tags = ['RandomSamplingStorage'];
-func.dependencies = ['Hub', 'Chronos', 'Token', 'KnowledgeCollectionStorage'];
+func.dependencies = ['Hub', 'Chronos'];
