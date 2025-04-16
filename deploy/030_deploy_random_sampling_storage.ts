0dbd0574fbc85ef387185a079c0b537160f1c10e
Zvonimir <zvonimir@Zvonimirs-MacBook-Pro.local>
2025-04-16 14:54:07 +0200

    Adapt RS deployments for all networks

--- deploy/030_deploy_random_sampling_storage.ts
+++ deploy/030_deploy_random_sampling_storage.ts
@@ -1,10 +1,22 @@
 import { HardhatRuntimeEnvironment } from 'hardhat/types';
 import { DeployFunction } from 'hardhat-deploy/types';
 
+type RandomSamplingStorageNetworkConfig = {
+  proofingPeriodDurationInBlocks: string;
+};
+
 const func: DeployFunction = async function (hre: HardhatRuntimeEnvironment) {
-  const randomSamplingStorageParametersConfig =
-    hre.helpers.parametersConfig[hre.network.config.environment]
-      .RandomSamplingStorage;
+  const randomSamplingStorageParametersConfig = hre.helpers.parametersConfig[
+    hre.network.config.environment
+  ].RandomSamplingStorage[
+    hre.network.name
+  ] as unknown as RandomSamplingStorageNetworkConfig;
+
+  if (!randomSamplingStorageParametersConfig) {
+    throw new Error(
+      `RandomSamplingStorage parameters config not found for network: ${hre.network.name}`,
+    );
+  }
 
   await hre.helpers.deploy({
     newContractName: 'RandomSamplingStorage',
