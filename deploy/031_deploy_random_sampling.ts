1807e5b00bd4847473fd047849b6d4ed233db257
Zvonimir <zvonimir@Zvonimirs-MacBook-Pro.local>
2025-04-16 14:54:07 +0200

    Adapt RS deployments for all networks

--- deploy/031_deploy_random_sampling.ts
+++ deploy/031_deploy_random_sampling.ts
@@ -1,9 +1,23 @@
 import { HardhatRuntimeEnvironment } from 'hardhat/types';
 import { DeployFunction } from 'hardhat-deploy/types';
 
+// Define an interface for the expected config structure
+type RandomSamplingNetworkConfig = {
+  avgBlockTimeInSeconds: string;
+  W1: string;
+  W2: string;
+};
+
 const func: DeployFunction = async function (hre: HardhatRuntimeEnvironment) {
-  const randomSamplingParametersConfig =
-    hre.helpers.parametersConfig[hre.network.config.environment].RandomSampling;
+  const randomSamplingParametersConfig = hre.helpers.parametersConfig[
+    hre.network.config.environment
+  ].RandomSampling[hre.network.name] as unknown as RandomSamplingNetworkConfig;
+
+  if (!randomSamplingParametersConfig) {
+    throw new Error(
+      `RandomSampling parameters config not found for network: ${hre.network.name}`,
+    );
+  }
 
   await hre.helpers.deploy({
     newContractName: 'RandomSampling',
@@ -28,4 +42,6 @@ func.dependencies = [
   'DelegatorsInfo',
   'KnowledgeCollectionStorage',
   'IdentityStorage',
+  'ShardingTableStorage',
+  'ParametersStorage',
 ];
