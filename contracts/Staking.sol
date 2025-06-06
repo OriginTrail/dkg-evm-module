1a2d6c7e654a5879c05f737be6e81da23059f3e4
FilipJoksimovic29 <153454929+FilipJoksimovic29@users.noreply.github.com>
2025-06-06 10:17:33 +0200

    updated claimDelegatorRewards, deleted two functions

--- contracts/Staking.sol
+++ contracts/Staking.sol
@@ -500,55 +500,6 @@ contract Staking is INamed, IVersioned, ContractStatus, IInitializable {
     //     return (delegatorStakeBase, delegatorStakeIndexed + additionalReward, additionalReward);
     // }
 
-    function getNetDelegatorsRewards(
-        uint72 identityId,
-        uint256 epoch
-    ) public view profileExists(identityId) returns (uint256) {
-        // If the operator fee has been claimed, return the net delegators rewards
-        if (delegatorsInfo.getIsOperatorFeeClaimedForEpoch(identityId, epoch)) {
-            return delegatorsInfo.getEpochLeftoverDelegatorsRewards(identityId, epoch);
-        }
-
-        uint256 nodeScore = randomSamplingStorage.getNodeEpochScore(epoch, identityId);
-        if (nodeScore == 0) return 0;
-
-        uint256 allNodesScore = randomSamplingStorage.getAllNodesEpochScore(epoch);
-        if (allNodesScore == 0) return 0;
-
-        uint256 epocRewardsPool = epochStorage.getEpochPool(1, epoch);
-        if (epocRewardsPool == 0) return 0;
-
-        uint256 delegatorsRewards = (epocRewardsPool * nodeScore) / allNodesScore;
-
-        uint256 feePercentageForEpoch = profileStorage.getLatestOperatorFeePercentage(identityId);
-        uint96 operatorFeeAmount = uint96((delegatorsRewards * feePercentageForEpoch) / 10000);
-
-        return delegatorsRewards - operatorFeeAmount;
-    }
-
-    function getNodeRewardsForEpoch(
-        uint72 identityId,
-        uint256 epoch
-    ) public view profileExists(identityId) returns (uint256) {
-        // If the operator fee has been claimed, return the net delegators rewards
-        if (delegatorsInfo.getIsOperatorFeeClaimedForEpoch(identityId, epoch)) {
-            return delegatorsInfo.getEpochLeftoverDelegatorsRewards(identityId, epoch);
-        }
-
-        uint256 nodeScore = randomSamplingStorage.getNodeEpochScore(epoch, identityId);
-        if (nodeScore == 0) return 0;
-
-        uint256 allNodesScore = randomSamplingStorage.getAllNodesEpochScore(epoch);
-        if (allNodesScore == 0) return 0;
-
-        uint256 epocRewardsPool = epochStorage.getEpochPool(1, epoch);
-        if (epocRewardsPool == 0) return 0;
-
-        uint256 nodeRewardsForEpoch = (epocRewardsPool * nodeScore) / allNodesScore;
-
-        return nodeRewardsForEpoch;
-    }
-
     function claimDelegatorRewards(
         uint72 identityId,
         uint256 epoch,
@@ -581,18 +532,18 @@ contract Staking is INamed, IVersioned, ContractStatus, IInitializable {
         uint256 delegatorScore = _prepareForStakeChange(epoch, identityId, delegatorKey);
         uint256 nodeScore = randomSamplingStorage.getNodeEpochScore(epoch, identityId);
         uint256 totalLeftoverEpochlRewardsForDelegators = 0;
-        uint256 nodeRewardsForEpoch = 0;
+        uint256 nodeDelegatorsRewardsForEpoch = 0;
 
         if (!delegatorsInfo.getIsOperatorFeeClaimedForEpoch(identityId, epoch)) {
             uint256 feePercentageForEpoch = profileStorage.getLatestOperatorFeePercentage(identityId);
             uint256 allNodesScore = randomSamplingStorage.getAllNodesEpochScore(epoch);
             if (allNodesScore != 0) {
                 uint256 epocRewardsPool = epochStorage.getEpochPool(1, epoch);
-                nodeRewardsForEpoch = (epocRewardsPool * nodeScore) / allNodesScore;
+                nodeDelegatorsRewardsForEpoch = (epocRewardsPool * nodeScore) / allNodesScore;
             }
 
-            uint96 operatorFeeAmount = uint96((nodeRewardsForEpoch * feePercentageForEpoch) / 10000);
-            totalLeftoverEpochlRewardsForDelegators = nodeRewardsForEpoch - operatorFeeAmount;
+            uint96 operatorFeeAmount = uint96((nodeDelegatorsRewardsForEpoch * feePercentageForEpoch) / 10000);
+            totalLeftoverEpochlRewardsForDelegators = nodeDelegatorsRewardsForEpoch - operatorFeeAmount;
             stakingStorage.increaseOperatorFeeBalance(identityId, operatorFeeAmount);
             delegatorsInfo.setIsOperatorFeeClaimedForEpoch(identityId, epoch, true);
             delegatorsInfo.setLastClaimedDelegatorsRewardsEpoch(identityId, epoch);
