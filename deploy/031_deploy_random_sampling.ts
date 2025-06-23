617f07894337d0282408d05cbb82ca17e0139e2a
Zvonimir <zvonimir@Zvonimirs-MacBook-Pro.local>
2025-06-23 12:39:20 +0200

    comment out deployments so tests work

--- deploy/031_deploy_random_sampling.ts
+++ deploy/031_deploy_random_sampling.ts
@@ -1,25 +1,25 @@
-// import { HardhatRuntimeEnvironment } from 'hardhat/types';
-// import { DeployFunction } from 'hardhat-deploy/types';
+import { HardhatRuntimeEnvironment } from 'hardhat/types';
+import { DeployFunction } from 'hardhat-deploy/types';
 
-// const func: DeployFunction = async function (hre: HardhatRuntimeEnvironment) {
-//   await hre.helpers.deploy({
-//     newContractName: 'RandomSampling',
-//   });
-// };
+const func: DeployFunction = async function (hre: HardhatRuntimeEnvironment) {
+  await hre.helpers.deploy({
+    newContractName: 'RandomSampling',
+  });
+};
 
-// export default func;
-// func.tags = ['RandomSampling'];
-// func.dependencies = [
-//   'Hub',
-//   'Chronos',
-//   'RandomSamplingStorage',
-//   'StakingStorage',
-//   'ProfileStorage',
-//   'EpochStorage',
-//   'AskStorage',
-//   'DelegatorsInfo',
-//   'KnowledgeCollectionStorage',
-//   'IdentityStorage',
-//   'ShardingTableStorage',
-//   'ParametersStorage',
-// ];
+export default func;
+func.tags = ['RandomSampling'];
+func.dependencies = [
+  'Hub',
+  'Chronos',
+  'RandomSamplingStorage',
+  'StakingStorage',
+  'ProfileStorage',
+  'EpochStorage',
+  'AskStorage',
+  'DelegatorsInfo',
+  'KnowledgeCollectionStorage',
+  'IdentityStorage',
+  'ShardingTableStorage',
+  'ParametersStorage',
+];
