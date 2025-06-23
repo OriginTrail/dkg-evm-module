e5f39cebb17382b58c2fd6643c0396ff41b11ba5
Zvonimir <zvonimir@Zvonimirs-MacBook-Pro.local>
2025-06-23 12:25:23 +0200

    comment out new deployments

--- deploy/022_deploy_random_sampling_storage.ts
+++ deploy/022_deploy_random_sampling_storage.ts
@@ -1,35 +1,35 @@
-import { HardhatRuntimeEnvironment } from 'hardhat/types';
-import { DeployFunction } from 'hardhat-deploy/types';
+// import { HardhatRuntimeEnvironment } from 'hardhat/types';
+// import { DeployFunction } from 'hardhat-deploy/types';
 
-type RandomSamplingStorageNetworkConfig = {
-  proofingPeriodDurationInBlocks: string;
-  W1: string;
-  W2: string;
-};
+// type RandomSamplingStorageNetworkConfig = {
+//   proofingPeriodDurationInBlocks: string;
+//   W1: string;
+//   W2: string;
+// };
 
-const func: DeployFunction = async function (hre: HardhatRuntimeEnvironment) {
-  const randomSamplingStorageParametersConfig = hre.helpers.parametersConfig[
-    hre.network.config.environment
-  ].RandomSamplingStorage[
-    hre.network.name
-  ] as unknown as RandomSamplingStorageNetworkConfig;
+// const func: DeployFunction = async function (hre: HardhatRuntimeEnvironment) {
+//   const randomSamplingStorageParametersConfig = hre.helpers.parametersConfig[
+//     hre.network.config.environment
+//   ].RandomSamplingStorage[
+//     hre.network.name
+//   ] as unknown as RandomSamplingStorageNetworkConfig;
 
-  if (!randomSamplingStorageParametersConfig) {
-    throw new Error(
-      `RandomSamplingStorage parameters config not found for network: ${hre.network.name}`,
-    );
-  }
+//   if (!randomSamplingStorageParametersConfig) {
+//     throw new Error(
+//       `RandomSamplingStorage parameters config not found for network: ${hre.network.name}`,
+//     );
+//   }
 
-  await hre.helpers.deploy({
-    newContractName: 'RandomSamplingStorage',
-    additionalArgs: [
-      randomSamplingStorageParametersConfig.proofingPeriodDurationInBlocks,
-      randomSamplingStorageParametersConfig.W1,
-      randomSamplingStorageParametersConfig.W2,
-    ],
-  });
-};
+//   await hre.helpers.deploy({
+//     newContractName: 'RandomSamplingStorage',
+//     additionalArgs: [
+//       randomSamplingStorageParametersConfig.proofingPeriodDurationInBlocks,
+//       randomSamplingStorageParametersConfig.W1,
+//       randomSamplingStorageParametersConfig.W2,
+//     ],
+//   });
+// };
 
-export default func;
-func.tags = ['RandomSamplingStorage'];
-func.dependencies = ['Hub'];
+// export default func;
+// func.tags = ['RandomSamplingStorage'];
+// func.dependencies = ['Hub'];
--- deploy/024_deploy_staking_kpi.ts
+++ deploy/024_deploy_staking_kpi.ts
@@ -1,21 +1,21 @@
-import { HardhatRuntimeEnvironment } from 'hardhat/types';
-import { DeployFunction } from 'hardhat-deploy/types';
+// import { HardhatRuntimeEnvironment } from 'hardhat/types';
+// import { DeployFunction } from 'hardhat-deploy/types';
 
-const func: DeployFunction = async function (hre: HardhatRuntimeEnvironment) {
-  await hre.helpers.deploy({
-    newContractName: 'StakingKPI',
-  });
-};
+// const func: DeployFunction = async function (hre: HardhatRuntimeEnvironment) {
+//   await hre.helpers.deploy({
+//     newContractName: 'StakingKPI',
+//   });
+// };
 
-export default func;
-func.tags = ['StakingKPI'];
-func.dependencies = [
-  'Hub',
-  'IdentityStorage',
-  'ProfileStorage',
-  'StakingStorage',
-  'DelegatorsInfo',
-  'RandomSamplingStorage',
-  'EpochStorage',
-  'ParametersStorage',
-];
+// export default func;
+// func.tags = ['StakingKPI'];
+// func.dependencies = [
+//   'Hub',
+//   'IdentityStorage',
+//   'ProfileStorage',
+//   'StakingStorage',
+//   'DelegatorsInfo',
+//   'RandomSamplingStorage',
+//   'EpochStorage',
+//   'ParametersStorage',
+// ];
