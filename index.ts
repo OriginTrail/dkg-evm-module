120f5bcf5fd4d3bde984e2981ad132f171ed3274
Zvonimir <zvonimir@Zvonimirs-MacBook-Pro.local>
2025-06-27 15:53:03 +0200

    new version and updates index.ts

--- index.ts
+++ index.ts
@@ -2,6 +2,7 @@ import Ask from './abi/Ask.json';
 import AskStorage from './abi/AskStorage.json';
 import Chronos from './abi/Chronos.json';
 import ContractStatus from './abi/ContractStatus.json';
+import DelegatorsInfo from './abi/DelegatorsInfo.json';
 import EpochStorage from './abi/EpochStorage.json';
 import Guardian from './abi/Guardian.json';
 import Hub from './abi/Hub.json';
@@ -14,25 +15,35 @@ import IERC20Extended from './abi/IERC20Extended.json';
 import IIndexable from './abi/IIndexable.json';
 import IInitializable from './abi/IInitializable.json';
 import INamed from './abi/INamed.json';
+import IParanetIncentivesPool from './abi/IParanetIncentivesPool.json';
 import IPaymaster from './abi/IPaymaster.json';
 import IVersioned from './abi/IVersioned.json';
 import KnowledgeCollection from './abi/KnowledgeCollection.json';
 import KnowledgeCollectionStorage from './abi/KnowledgeCollectionStorage.json';
 import Migrator from './abi/Migrator.json';
+import MigratorM1V8 from './abi/MigratorM1V8.json';
+import MigratorM1V8_1 from './abi/MigratorM1V8_1.json';
 import ParametersStorage from './abi/ParametersStorage.json';
 import Paranet from './abi/Paranet.json';
+import ParanetIncentivesPool from './abi/ParanetIncentivesPool.json';
 import ParanetIncentivesPoolFactory from './abi/ParanetIncentivesPoolFactory.json';
+import ParanetIncentivesPoolFactoryHelper from './abi/ParanetIncentivesPoolFactoryHelper.json';
+import ParanetIncentivesPoolStorage from './abi/ParanetIncentivesPoolStorage.json';
 import ParanetKnowledgeCollectionsRegistry from './abi/ParanetKnowledgeCollectionsRegistry.json';
 import ParanetKnowledgeMinersRegistry from './abi/ParanetKnowledgeMinersRegistry.json';
 import ParanetServicesRegistry from './abi/ParanetServicesRegistry.json';
 import ParanetsRegistry from './abi/ParanetsRegistry.json';
+import ParanetStagingRegistry from './abi/ParanetStagingRegistry.json';
 import Paymaster from './abi/Paymaster.json';
 import PaymasterManager from './abi/PaymasterManager.json';
 import Profile from './abi/Profile.json';
 import ProfileStorage from './abi/ProfileStorage.json';
+import RandomSampling from './abi/RandomSampling.json';
+import RandomSamplingStorage from './abi/RandomSamplingStorage.json';
 import ShardingTable from './abi/ShardingTable.json';
 import ShardingTableStorage from './abi/ShardingTableStorage.json';
 import Staking from './abi/Staking.json';
+import StakingKPI from './abi/StakingKPI.json';
 import StakingStorage from './abi/StakingStorage.json';
 import Token from './abi/Token.json';
 import WhitelistStorage from './abi/WhitelistStorage.json';
@@ -48,11 +59,15 @@ export {
   IdentityStorage as IdentityStorageABI,
   Identity as IdentityABI,
   Paranet as ParanetABI,
+  ParanetIncentivesPool as ParanetIncentivesPoolABI,
   ParanetIncentivesPoolFactory as ParanetIncentivesPoolFactoryABI,
+  ParanetIncentivesPoolFactoryHelper as ParanetIncentivesPoolFactoryHelperABI,
+  ParanetIncentivesPoolStorage as ParanetIncentivesPoolStorageABI,
   ParanetKnowledgeCollectionsRegistry as ParanetKnowledgeCollectionsRegistryABI,
   ParanetKnowledgeMinersRegistry as ParanetKnowledgeMinersRegistryABI,
   ParanetServicesRegistry as ParanetServicesRegistryABI,
   ParanetsRegistry as ParanetsRegistryABI,
+  ParanetStagingRegistry as ParanetStagingRegistryABI,
   ProfileStorage as ProfileStorageABI,
   Profile as ProfileABI,
   StakingStorage as StakingStorageABI,
@@ -74,6 +89,13 @@ export {
   INamed as INamedABI,
   IVersioned as IVersionedABI,
   IContractStatus as IContractStatusABI,
+  IParanetIncentivesPool as IParanetIncentivesPoolABI,
   IPaymaster as IPaymasterABI,
   IERC20Extended as IERC20ExtendedABI,
+  DelegatorsInfo as DelegatorsInfoABI,
+  RandomSampling as RandomSamplingABI,
+  RandomSamplingStorage as RandomSamplingStorageABI,
+  StakingKPI as StakingKPIABI,
+  MigratorM1V8 as MigratorM1V8ABI,
+  MigratorM1V8_1 as MigratorM1V8_1ABI,
 };
