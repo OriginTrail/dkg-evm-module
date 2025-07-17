25e41493071a573e667ededbd0f1cc2123883515
Zvonimir <zvonimir@Zvonimirs-MacBook-Pro.local>
2025-07-17 16:17:51 +0200

    adapt initialize script to impersonate hub owner

--- deploy/998_initialize_contracts.ts
+++ deploy/998_initialize_contracts.ts
@@ -1,6 +1,8 @@
 import { HardhatRuntimeEnvironment } from 'hardhat/types';
 import { DeployFunction } from 'hardhat-deploy/types';
 
+import { HUB_OWNERS, NETWORK_HUBS } from '../constants/simulation-constants';
+
 const func: DeployFunction = async function (hre: HardhatRuntimeEnvironment) {
   const {
     newContracts,
@@ -40,20 +42,48 @@ const func: DeployFunction = async function (hre: HardhatRuntimeEnvironment) {
     !noChangesWereMade &&
     !['development'].includes(hre.network.config.environment)
   ) {
-    const hubAddress =
-      hre.helpers.contractDeployments.contracts['Hub'].evmAddress;
-
-    console.log(`Hub: ${hubAddress}`);
+    // Get Hub address from hardhat-deploy registry (where we registered it in 000_setup_existing_hub.ts)
+    const hubDeployment = await hre.deployments.get('Hub');
+    const hubAddress = hubDeployment.address;
 
     const Hub = await hre.ethers.getContractAt('Hub', hubAddress);
 
-    const setAndReinitializeContractsTx = await Hub.setAndReinitializeContracts(
+    const hubOwner = HUB_OWNERS[hubAddress as keyof typeof HUB_OWNERS];
+    if (!hubOwner) {
+      throw new Error(`Hub owner not found for Hub address: ${hubAddress}`);
+    }
+    console.log(
+      `[998 DEPLOYMENT] Hub owner: ${hubOwner} for Hub address: ${hubAddress} on network: ${NETWORK_HUBS[hubAddress as keyof typeof NETWORK_HUBS]}`,
+    );
+
+    // Fund the hub owner account with ETH for transaction fees
+    const { deployer } = await hre.getNamedAccounts();
+    const deployerSigner = await hre.ethers.getSigner(deployer);
+    const fundingAmount = hre.ethers.parseEther('1.0'); // 1 ETH should be enough
+
+    console.log(
+      `[998 DEPLOYMENT] Funding hub owner ${hubOwner} with ${hre.ethers.formatEther(fundingAmount)} ETH...`,
+    );
+    await deployerSigner.sendTransaction({
+      to: hubOwner,
+      value: fundingAmount,
+    });
+
+    await hre.network.provider.send('hardhat_impersonateAccount', [hubOwner]);
+
+    const setAndReinitializeContractsTx = await Hub.connect(
+      await hre.ethers.getSigner(hubOwner),
+    ).setAndReinitializeContracts(
       newContracts,
       newAssetStorageContracts,
       contractsForReinitialization,
       setParametersEncodedData,
     );
     await setAndReinitializeContractsTx.wait();
+
+    await hre.network.provider.send('hardhat_stopImpersonatingAccount', [
+      hubOwner,
+    ]);
   }
 };
 
