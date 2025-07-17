382f3d7b78ad654b57fa3107fcdb6e03e771a51d
Zvonimir <zvonimir@Zvonimirs-MacBook-Pro.local>
2025-07-17 14:14:25 +0200

    disable unnecessary deployment scripts

--- disabled_deploy/003_deploy_parameters_storage.ts
+++ disabled_deploy/003_deploy_parameters_storage.ts
@@ -6,19 +6,20 @@ type ParametersStorageConfig = {
 };
 
 const func: DeployFunction = async function (hre: HardhatRuntimeEnvironment) {
+  // eslint-disable-next-line @typescript-eslint/no-unused-vars
   const parametersStorageParametersConfig = hre.helpers.parametersConfig[
     hre.network.config.environment
   ].ParametersStorage as unknown as ParametersStorageConfig;
 
-  if (!parametersStorageParametersConfig?.v81ReleaseEpoch) {
-    throw new Error(
-      `v81ReleaseEpoch not found in parameters config for network: ${hre.network.name}`,
-    );
-  }
+  // if (!parametersStorageParametersConfig?.v81ReleaseEpoch) {
+  //   throw new Error(
+  //     `v81ReleaseEpoch not found in parameters config for network: ${hre.network.name}`,
+  //   );
+  // }
 
   const ParametersStorage = await hre.helpers.deploy({
     newContractName: 'ParametersStorage',
-    additionalArgs: [parametersStorageParametersConfig.v81ReleaseEpoch],
+    // additionalArgs: [parametersStorageParametersConfig.v81ReleaseEpoch],
   });
 
   await hre.helpers.updateContractParameters(
